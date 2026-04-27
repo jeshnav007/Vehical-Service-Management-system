9f0cfeb9c2293c1229c83b362b1f107f4585e7c4
jeshnav velagapudi <jeshnavvelagapudi07@gmail.com>
2026-04-28 00:52:59 +0530

    Update URLs for production deployment

--- server/server.js
+++ server/server.js
@@ -19,7 +19,17 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173', process.env.FRONTEND_URL];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
